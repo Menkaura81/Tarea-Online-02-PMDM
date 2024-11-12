169937659b84b6f6532890bda7adc65f6b820345
Menkaura81 <bertoalarcon@gmail.com>
2024-11-12 20:14:00 +0100

    primera clase

--- app/src/main/java/com/example/tarea02aal/MainActivity.java
+++ app/src/main/java/com/example/tarea02aal/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.tarea02aal;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +14,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.tarea02aal.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Añadimos el binding
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        // Añadimos eventos
+        binding.acceptButton.setOnClickListener((this::acceptPressed));
+        // acceptButton.setOnClickListener(this::acceptPressed);
+        
+    }
+
+    private void acceptPressed(View view) {
+        binding.img.setVisibility(View.GONE);
+        System.out.println("Button Pressed");
+        Toast.makeText(this, "Button Pressed", Toast.LENGTH_SHORT).show();
     }
 }
